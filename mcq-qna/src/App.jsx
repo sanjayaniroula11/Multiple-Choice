6627545caecc40fefaddd98cd36e81e627e26e13
Sanjaya Niroula <niroulasanjaya42@fmail.com>
2024-05-07 18:23:50 +0545

    ...rem

--- mcq-qna/src/App.jsx
+++ mcq-qna/src/App.jsx
@@ -5,28 +5,59 @@ import Resources from './question.json'
 import './App.css'
 
 function App() {
-  const [questionIndex, setquestionIndex] = useState(0);
 
- const prip = () => console.log(Resources);
+  const [currQuestion, setCurrQuestion] = useState(0);
+  const [answer, setAnswer] = useState(0);
 
+const nextQuestion = ()=>{
+  if (currQuestion < Resources.length-1){
+    setCurrQuestion( currQuestion + 1 );
+    setAnswer(0);
+  }
+  else {
+    
+  }
 
+} 
+
+const checkAnswer = ()=> {
+  if (answer === Resources[currQuestion].correctAnswer){
+    
+  }
+}
+
+const highlightAnswer = ()=>{
+  if ( option === Resources[currQuestion].correctAnswer){
+
+  }
+}
 
   return (
     <>
     <div className="main">
     <div className="app">
-      <h1>Start Playing</h1>
+      <h1>GENERAL KNOWLEDGE</h1>
       <div className="question">
-        <h3>1. Guess the correct height of mt. Makalu. </h3>
+        <h3>{currQuestion + 1}. {Resources[currQuestion].question} </h3>
         <ul>
-          <li>8465m</li>
-          <li>8485m</li>
+          {Resources[currQuestion].choices.map((option,indexOfOption) => {
+
+            return <li
+            className={`answer ${answer == indexOfOption+1 ? 'answer-select' : null}`}
+            onClick={()=>{
+              setAnswer(indexOfOption + 1 );
+            }}
+            >{option}</li>
+
+    
+          })}
+          {/* <li>8485m</li>
           <li>6725m</li>
-          <li>None of the above</li>
+          <li>None of the above</li> */}
         </ul>
       </div>
         <button 
-        onClick={prip}
+        onClick={nextQuestion}
         className="next">
           Next
         </button>
